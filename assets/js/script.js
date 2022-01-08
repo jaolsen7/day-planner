8dbe7f246a84414446c5261697c898c8a944ddba
jaolsen7 <jaolsen@berkeley.edu>
2022-01-07 19:48:05 -0800

    text area inputted with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@ function displayDay () {
     var currentTime = time.format("[Today's date is] MMM Do, YYYY.");
     $("#currentDay").text(currentTime);
 }
-
 setInterval(displayDay, 1000);
+
+for (var i = 9; i < 18; i++) {
+    var stringi = String(i);
+    var storedData = localStorage.getItem(stringi);
+    var targetid = "hour-" + stringi;
+    $("#" + targetid).children().eq(1).text(storedData);
+}
 
 var saveBtn = $(".saveBtn");
 var parentEl = saveBtn.parent();
@@ -14,7 +20,7 @@ $(".time-block").each(function() {
     var hour = $(this).attr("id").split("-");
     var hourNum = hour[1];
     var hourParse = parseInt(hourNum);
-    
+
     if (hourParse < time.hour()) {
         $(this).addClass("past")
     }
@@ -27,7 +33,8 @@ $(".time-block").each(function() {
 });
 
 saveBtn.on("click", function(event) {
-    var hour = $(this).parent().attr("id");
+    var hour = $(this).parent().attr("id").split("-");
+    var hourNum = hour[1];
     var text = $(this).siblings(".description").val();
-    localStorage.setItem(hour, text); 
+    localStorage.setItem(hourNum, text); 
 });
